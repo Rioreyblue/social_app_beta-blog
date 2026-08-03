ce9108985792982518373bb86f764fb2736813e4
Rioreyblue <franciscorey702@gmail.com>
2026-08-03 23:50:02 +0800

    replaced the main code file to import to app/src

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+import uvicorn
 
-app = FastAPI()
-
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Corey Schafer",
-        "title": "FastAPI is Awesome",
-        "content": "This framework is really easy to use and super fast.",
-        "date_posted": "April 20, 2025",
-    },
-    {
-        "id": 2,
-        "author": "Jane Doe",
-        "title": "Python is Great for Web Development",
-        "content": "Python is a great language for web development, and FastAPI makes it even better",
-        "date_posted": "April 21, 2025",
-    },
-]
-
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
-
-# api endpoint
-@app.get("/api/posts")
-def get_posts():
-    return posts
+if __name__ == "__main__":
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
