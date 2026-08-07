b10381a0de04d9735d6932520929bff1db07dbee
Rioreyblue <franciscorey702@gmail.com>
2026-08-07 22:17:38 +0800

    return list instead of individual id

--- app/app_child.py
+++ app/app_child.py
@@ -62,7 +62,8 @@ text_posts = {
 @ReyApp.get("/posts")
 def get_all_posts(limit: int = None):
     if limit:
-        return text_posts[:limit]
+        # return text_posts[:limit]
+        return list(text_posts.values())[:limit]
     return text_posts
 
 @ReyApp.get("/posts/{id}")
