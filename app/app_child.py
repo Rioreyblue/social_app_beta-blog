df492f137e2195719579eb8baf1c95ad7051bfcd
Rioreyblue <franciscorey702@gmail.com>
2026-08-10 23:34:13 +0800

    adding PostResponse

--- app/app_child.py
+++ app/app_child.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from app.schemas import PostCreate
+from app.schemas import PostCreate, PostResponse
 
 ReyApp = FastAPI()
 
@@ -81,7 +81,7 @@ def get_post(id: int):
 #     return text_posts.get[post_id]
 
 @ReyApp.post("/posts")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostResponse:
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys())+1] = new_post
     return new_post
