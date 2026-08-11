265a942faf149bc81138bbf50de4e8dc368ce5fc
Rioreyblue <franciscorey702@gmail.com>
2026-08-11 23:54:54 +0800

    add local database setup

--- app/db.py
+++ app/db.py
@@ -5,3 +5,5 @@ from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
+
+DATABASE_URL = "sqlite+aioslite:///./test.db"
