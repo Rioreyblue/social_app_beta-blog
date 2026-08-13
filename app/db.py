d7a9910585458ce6aed0588bb3e454dd3a95b99d
Rioreyblue <franciscorey702@gmail.com>
2026-08-14 00:01:04 +0800

    add session maker  functiuon db

--- app/db.py
+++ app/db.py
@@ -25,3 +25,8 @@ async_sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
 async def create_dd_and_tables():
     async with engine.begin as conn:
         await conn.run_sync(DeclarativeBase.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
