875ccb912d6207f4bf27928b076abaf17b014b8b
Rioreyblue <franciscorey702@gmail.com>
2026-08-14 23:53:40 +0800

    last call mispelled AsysncSessioin and Begin method with no ()

--- app/app_child.py
+++ app/app_child.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
-from sqlalchemy.ext.asyncio import AsyncnSession
+from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
