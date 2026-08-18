cc825e3739179df784b4de5a965c4d1323f2cefc
Rioreyblue <franciscorey702@gmail.com>
2026-08-18 23:50:09 +0800

    importing my keys for image kit

--- app/images.py
+++ app/images.py
@@ -4,4 +4,8 @@ import os
 
 load_dotenv()
 
-imagekit = ImageKit()
+imagekit = ImageKit(
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
+    public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
+    url_endpoint=os.getenv("IMAGEKIT_URL"),
+)
